04e4a907c1d6f0c0ff4086ecdec0e28037774c56
Filip Eddebring <fied1225@storsjogymnasiet.se>
2023-05-23 10:25:17 +0100

    klar

--- main.py
+++ main.py
@@ -3,8 +3,31 @@ import os
 import jokehandler
 
 def main():
-    print("hej")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    print("\n-Torra historier-")
+
+    url = f"https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+    nr = 1
+    jokeobjekt = jokehandler.Jokehandler(url)
+
+    while True:
+
+        tJoke = jokeobjekt.getJoke()
+
+        print(f"\n{nr}.--------------------------------")
+        print(f"{tJoke}")
+        print("----------------------------------------")
+
+        nr += 1
+
+        entill = input("Vill du ha en till? Y/N ")
+
+        if (entill == "n" or entill == "N"):
+            break
+
+        
 
 
 main()
